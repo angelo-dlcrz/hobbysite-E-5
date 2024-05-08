22df5a81ff72b581509d71e3750641a411f2f7cb
Aster Benedict A. Mangabat <aster.mangabat@student.ateneo.edu>
2024-05-08 19:56:26 +0800

    updated commission urls

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import CommissionListView, CommissionDetailView
+from .views import CommissionListView, CommissionDetailView, CommissionCreateView, JobCreateView, CommissionUpdateView, JobUpdateView, JobApplicationUpdateView
 
 
 urlpatterns = [
     path('list', CommissionListView.as_view(), name="list"),
-    path('detail/<int:pk>', CommissionDetailView.as_view(), name="commission_detail")
+    path('detail/<int:pk>', CommissionDetailView.as_view(),
+         name="commission_detail"),
+    path('add', CommissionCreateView.as_view(), name="add"),
+    path('detail/<int:pk>/add', JobCreateView.as_view(), name="add_job"),
+    path('detail/<int:pk>/update', CommissionUpdateView.as_view(), name="update"),
+    path('jobs/<int:pk>/edit/', JobUpdateView.as_view(), name="update_job"),
+    path('commissions/<int:commission_pk>/jobs/<int:job_pk>/applications/<int:pk>/update/', JobApplicationUpdateView.as_view(), name="update_job_application"),
+
 ]
 
 app_name = 'commissions'
