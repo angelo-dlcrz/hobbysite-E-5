e6d192d0b52dfc2deb86e7bd5b76d8180b789b1b
kingteng20 <james.abarca@student.ateneo.edu>
2024-05-08 14:55:13 +0800

    Final merchstore app, list done

---
+++ merchstore/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib import admin
+
+from .models import Product, Transaction
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        widgets = {
+            'product_type': forms.Select(),
+            'status': forms.Select(),
+        }
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ('status', )
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,14 +1,43 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
-from .models import Product
+from .models import Product, ProductType, Transaction
+from .forms import ProductForm, TransactionForm
+
+from user_management.models import Profile
 
 
 class MerchListView(ListView):
     model = Product
     template_name = 'merch_list.html'
 
+    def get_context_date(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['product_types'] = ProductType.objects.all()
+        return ctx
+
 
 class MerchDetailView(DetailView):
     model = Product
     template_name = 'merch_detail.html'
+
+
+class MerchCreateView(CreateView):
+    model = Product
+    template_name = 'merch_create.html'
+
+
+class MerchUpdateView(UpdateView):
+    model = Product
+    template_name = 'merch_update.html'
+
+
+class CartView(ListView):
+    model = Transaction
+    template_name = 'cart.html'
+
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'transaction_list.html'
