dd29d6f4cde65b276aab3107fcea803c0ab1b0e4
Aster Benedict A. Mangabat <aster.mangabat@student.ateneo.edu>
2024-05-08 19:55:42 +0800

    updated models

--- commissions/models.py
+++ commissions/models.py
@@ -1,42 +1,96 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
+
 
 class Commission(models.Model):
+    commission_status_options = (
+        ("OPEN", "OPEN"),
+        ("FULL", "FULL"),
+        ("ECOMPLETED", "COMPLETED"),
+        ("DISCONTINUED", "DISCONTINUED")
+    )
     title = models.CharField(max_length=255)
     description = models.TextField()
-    people_required = models.PositiveIntegerField()
+    commission_status = models.CharField(
+        max_length=15,
+        choices=commission_status_options,
+        default="OPEN"
+    )
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="profile")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse('commissions:commission_detail', args=[self.pk])
-    
+
     def get_first_words(self):
         return self.description[:500]
-    
+
     def exceeds(self):
-        return len(self.description)>500
-    
+        return len(self.description) > 500
+
     class Meta:
-        ordering = ['created_on']
+        ordering = ['-commission_status', '-created_on']
+    
+    def get_commission_status(self):
+        if self.commission_status=="ECOMPLETED":
+            return "COMPLETED"
+        else:
+            return self.commission_status
 
 
-class Comment(models.Model):
+class Job(models.Model):
     commission = models.ForeignKey(
         'Commission',
-        on_delete = models.CASCADE,
-        related_name = 'comments'
+        on_delete=models.CASCADE,
+        related_name='jobs'
     )
-    entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=255)
+    manpower = models.PositiveIntegerField()
+    job_status_options = (
+        ("OPEN", "OPEN"),
+        ("FULL", "FULL")
+    )
+    job_status = models.CharField(
+        max_length=10, choices=job_status_options, default="open")
 
     def __str__(self):
-        return "{} - {}".format(self.commission.title, str(self.created_on))
-    
+        return self.role
+
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['job_status', '-manpower', 'role']
+
+
+class JobApplication (models.Model):
+    job = models.ForeignKey(
+        'Job',
+        on_delete=models.CASCADE,
+        related_name='applications'
+    )
+    applicant = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="applicants"
+    )
+    application_status_options = (
+        ("PENDING", "PENDING"),
+        ("ACCEPTED", "ACCEPTED"),
+        ("REJECTED", "REJECTED")
+    )
+    application_status = models.CharField(
+        max_length=10, choices=application_status_options, default="PENDING")
+    applied_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} - {}".format(str(self.applicant.display_name), str(self.job.role))
+
+    class Meta:
+        ordering = ['application_status', '-applied_on']
