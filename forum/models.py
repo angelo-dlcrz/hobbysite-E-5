83b6b421b8447f39364014e668a6e7cffa62eeab
DustinAgner27 <neil.agner@student.ateneo.edu>
2024-05-08 18:59:29 +0800

    Updated forum app

--- forum/models.py
+++ forum/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
 
-class PostCategory(models.Model):
+
+class ThreadCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
@@ -11,12 +13,15 @@ class PostCategory(models.Model):
 
     class Meta:
         ordering = ['name']
+        verbose_name_plural = "Thread Categories"
 
 
-class Post(models.Model):
+class Thread(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(PostCategory, on_delete=models.CASCADE, related_name='category')
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='thread_author')
+    category = models.ForeignKey(ThreadCategory, on_delete=models.SET_NULL, null=True, related_name='category')
     entry = models.TextField()
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -29,3 +34,16 @@ class Post(models.Model):
     class Meta:
         ordering = ['-created_on']
 
+
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name='comment_author')
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='thread')
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.entry
+    
+    class Meta:
+        ordering = ['created_on']
--- forum/views.py
+++ forum/views.py
@@ -1,13 +1,120 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Post
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+from .models import Thread, ThreadCategory, Comment
+
+from .forms import ThreadForm, ThreadUpdateForm, CommentForm
 
 
-class ForumListView(ListView):
-    model = Post
+class ThreadListView(ListView):
+    model = Thread
     template_name = 'forum_list.html'
+    context_object_name = 'threads_context'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = ThreadCategory.objects.all()
+        threads_category_dict = {}
+
+        if self.request.user.is_authenticated:
+            user = self.request.user.profile
+            for category in categories:
+                threads_category_dict[category] = Thread.objects.filter(category=category).exclude(author=user)
+            context['user_threads'] = Thread.objects.filter(author=user)
+            context['threads_category'] = threads_category_dict
+        else:
+            for category in categories:
+                threads_category_dict[category] = Thread.objects.filter(category=category)
+            context['threads_category'] = threads_category_dict
+        
+        return context
 
 
-class ForumDetailView(DetailView):
-    model = Post
+class ThreadDetailView(DetailView):
+    model = Thread
     template_name = 'forum_detail.html'
+    context_object_name = 'thread'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['more_threads'] = Thread.objects.filter(category=self.object.category).exclude(pk=self.object.pk)[:2]
+        context['comments'] = Comment.objects.filter(thread=self.object)
+        context['comments_form'] = CommentForm()
+
+        if self.request.user == self.object.author.user:
+            context['is_owner'] = True
+        else:
+            context['is_owner'] = False
+
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            if request.user.is_authenticated:
+                comment = form.save(commit=False)
+                comment.author = request.user.profile
+                comment.thread = self.object
+                comment.save()
+        return self.get(request, *args, **kwargs)
+    
+
+class ThreadCreateView(LoginRequiredMixin, CreateView):
+    model = Thread
+    fields = '__all__'
+    template_name = 'forum_create.html'
+    context_object_name = 'thread'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ThreadForm()
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ThreadForm(request.POST, request.FILES)
+        if form.is_valid():
+            if request.user.is_authenticated:
+                thread = form.save(commit=False)
+                thread.author = request.user.profile
+                thread.save()
+                print(thread.pk)
+                return redirect('forum:thread', pk=thread.pk)
+        return self.get(request, *args, **kwargs)
+    
+
+class ThreadUpdateView(LoginRequiredMixin, UpdateView):
+    model = Thread
+    fields = '__all__'
+    template_name = 'forum_update.html'
+    context_object_name = 'thread'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = ThreadUpdateForm()
+
+        if self.request.user == self.object.author.user:
+            context['is_owner'] = True
+        else:
+            context['is_owner'] = False
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = ThreadUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            if request.user.is_authenticated:
+                thread = form.save(commit=False)                
+                thread_old = Thread.objects.get(pk=self.get_object().pk)
+                thread_old.title = thread.title
+                thread_old.category = thread.category
+                thread_old.entry = thread.entry
+                thread_old.image = thread.image
+                thread_old.save()
+                return redirect('forum:thread', pk=thread_old.pk)
+        return self.get(request, *args, **kwargs)
