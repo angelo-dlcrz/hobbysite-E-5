2359773f9e1693ffef3d6bf0bf17ebbc25b6a6f0
angelo-dlcrz <angelo.delacruz@student.ateneo.edu>
2024-05-08 14:56:50 +0800

    Added all needed models for wiki app

--- wiki/models.py
+++ wiki/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -16,6 +17,12 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        null=True,
+        on_delete = models.SET_NULL,
+        related_name='article_author',
+    )
     category = models.ForeignKey(
         'ArticleCategory',
         null = True,
@@ -23,6 +30,10 @@ class Article(models.Model):
         related_name = 'categories',
     )
     entry = models.TextField()
+    header_image = models.ImageField(
+        upload_to="images/",
+        null=True,
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -34,3 +45,23 @@ class Article(models.Model):
     
     class Meta:
         ordering = ['-created_on'] 
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Profile,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='comment_author',
+    )
+    article = models.ForeignKey(
+        'Article',
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='article',
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on'] 
